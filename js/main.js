486a12764b951fec05bfe695ae57673d8916f0ab
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-22 01:08:48 +0100

    Learning the Javascript Document Object Model

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // DOM - Document Object Model
+const view1 = document.getElementById("view1");
+console.log(view1);
+
+//another way
+const view2 = document.querySelector("#view2");
+console.log(view2);
+
+//to change styles
+view1.style.display = "flex";
+view2.style.display = "flex";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
+const sameViews = document.querySelectorAll(".views");
+console.log(sameViews);
