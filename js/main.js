9c84ddb668ca866dadf84c50484231cc127e27ef
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-22 10:39:01 +0100

    Learning the Javascript Document Object Model

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ const views = document.getElementsByClassName("view");
 console.log(views);
 const sameViews = document.querySelectorAll(".views");
 console.log(sameViews);
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+const sameDivs = view1.getElementsByTagName("div");
+console.log(sameDivs);
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(evenDivs);
+for (let i = 0; i < evenDivs.length; i++) {
+  evenDivs[i].style.backgroundColor = "darkblue";
+  /*evenDivs[i].style.width = "200px";
+    evenDivs[i].style.height = "200px"; */
+}
+const navText = document.querySelector("nav h1");
+console.log(navText);
+navText.textContent = "Hello world!";
